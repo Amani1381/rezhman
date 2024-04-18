83ef1fd8c1bad5ea5f2d8d222ca98b2b9a08c007
Amani1381 <amaniadel1381@gmail.com>
2024-04-17 23:38:34 -0700

    adell

--- app/Models/ImageUpload.php
+++ app/Models/ImageUpload.php
@@ -5,15 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Blog extends Model
-
+class ImageUpload extends Model
 {
-    public function blog()
-    {
-
-        return $this->belongsTo('blog::class');
-
-
-
-    }
+    use HasFactory;
 }
