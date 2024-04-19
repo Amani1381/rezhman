c74ec4174099e70ecc448d653aa9a46f87985b61
hawre18 <51228188+hawre18@users.noreply.github.com>
2024-04-20 00:47:47 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\BlogController;
 
-Route::get('/', function () {
-    return view('index.v1.users.index');
-});
-//@@@@@@@
 
-Route::group(['prefix'=>'admin','namespace'=>'App\Http\Controllers\Admin'],function (){
-    Route::resource('categories','CategoryController');
-    Route::Patch('categories/update/{id}','CategoryController@update')->name('update.category');
-    Route::get('categories/delete/{id}','CategoryController@delete')->name('categories.delete');
-    Route::get('blog/delete/{id}','BlogController@delete')->name('blog.delete');
-    Route::resource('blogs','BlogController');
     });
